4ef0b6e520347b4b7d0f6311ea445c7418d04930
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-04-15 23:35:08 +0300

    add logic to fix call to other controllers (WIP)

---
+++ tests/Traits/DispatchAwareTraitTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Nip\Controllers\Tests\Traits;
+
+use Nip\Controllers\Tests\AbstractTest;
+use Nip\Controllers\Tests\Fixtures\BaseControllerWithUtilityMethods;
+
+/**
+ * Class DispatchAwareTraitTest
+ * @package Nip\Controllers\Tests\Traits
+ */
+class DispatchAwareTraitTest extends AbstractTest
+{
+    public function testDynamicCallHelper()
+    {
+        $controller = new BaseControllerWithUtilityMethods();
+
+        $controller->call();
+        static::assertInstanceOf('Nip_Helper_Url', $controller->Url());
+    }
+}
---
+++ tests/fixtures/BaseController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Nip\Controllers\Tests\Fixtures;
+
+use Nip\Controllers\Controller;
+use Nip\Controllers\Traits\BaseControllerTrait;
+
+/**
+ * Class BaseController
+ * @package Nip\Controllers\Tests\Fixtures
+ */
+class BaseController extends Controller
+{
+
+    public function index()
+    {
+        return 'index';
+    }
+
+}
