f4db0f0997dd0474cb2f43b3ab8fba91f2bff79e
Gabriel Solomon <hello@gabrielsolomon.ro>
2018-12-05 13:35:08 +0200

    add controller legacy

---
+++ legacy/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Nip;
+
+/**
+ * Class Controller
+ * @package Nip
+ * @deprecated use \Nip\Controllers\Controller
+ */
+class Controller extends \Nip\Controllers\Controller
+{
+}
